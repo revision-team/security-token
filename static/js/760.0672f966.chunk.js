7d630e666435ee16b0516348ac62e8179f565c3a
Amaury Diaz <amauryuh@gmail.com>
2023-07-11 15:56:39 +0200

    Updates

---
+++ static/js/760.0672f966.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkweb3modal_wallet=self.webpackChunkweb3modal_wallet||[]).push([[760],{7760:function(e,o,t){t.r(o);var n=null;"undefined"!==typeof WebSocket?n=WebSocket:"undefined"!==typeof MozWebSocket?n=MozWebSocket:"undefined"!==typeof t.g?n=t.g.WebSocket||t.g.MozWebSocket:"undefined"!==typeof window?n=window.WebSocket||window.MozWebSocket:"undefined"!==typeof self&&(n=self.WebSocket||self.MozWebSocket),o.default=n}}]);
+//# sourceMappingURL=760.0672f966.chunk.js.map
